b96024037e26f98402355f524323fd4d179a38db
cateiru <git@cateiru.com>
2025-10-19 22:17:27 +0900

    update

--- lib/FloatingActionButton.tsx
+++ lib/FloatingActionButton.tsx
@@ -1,19 +1,19 @@
 import type { Size } from "./utils/size";
 
 type FloatingActionButtonProps = {
-	onClick?: () => void;
+  onClick?: () => void;
 
-	ariaLabel?: string;
-	icon?: React.ReactNode;
-	color?: string;
-	backgroundColor?: string;
-	size?: Size;
+  ariaLabel?: string;
+  icon?: React.ReactNode;
+  color?: string;
+  backgroundColor?: string;
+  size?: Size;
 };
 
 export const FloatingActionButton = (props: FloatingActionButtonProps) => {
-	return (
-		<div>
-			<button aria-label={props.ariaLabel} onClick={props.onClick}></button>
-		</div>
-	);
+  return (
+    <div>
+      <button aria-label={props.ariaLabel} onClick={props.onClick}></button>
+    </div>
+  );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,21 @@ import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-	plugins: [react(), dts({ insertTypesEntry: true })],
-	build: {
-		lib: {
-			entry: "./lib/main.tsx",
-			name: "react-help-window",
-			fileName: "index",
-		},
-		rollupOptions: {
-			external: ["react", "react-dom"],
-			output: {
-				globals: {
-					react: "React",
-					"react-dom": "ReactDOM",
-				},
-			},
-		},
-	},
+  plugins: [react(), dts({ insertTypesEntry: true })],
+  build: {
+    lib: {
+      entry: "./lib/main.tsx",
+      name: "react-help-window",
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
 });
