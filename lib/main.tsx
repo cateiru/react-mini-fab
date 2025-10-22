17a22d57128b1fd588e03beb2c3d6c970065a4a2
Cateiru <git@cateiru.com>
2025-10-22 23:02:45 +0900

    format

--- lib/main.tsx
+++ lib/main.tsx
@@ -1,3 +1,3 @@
+export { useTemporaryFlag } from "./hooks/useTemporaryFlag";
 export { MiniFAB, type MiniFABProps } from "./MiniFAB";
 export { Draggable, type DraggableProps } from "./utils/Draggable";
-export { useTemporaryFlag } from "./hooks/useTemporaryFlag";
